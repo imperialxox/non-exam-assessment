64111d09da97bd2d51d2854e399f9f39d3cc811e
imperialxox <63364753+imperialxox@users.noreply.github.com>
2020-04-08 18:11:03 +0100

    Add files via upload

---
+++ NEA Project - Keanu/database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+public class EmailRecord
+{
+    [BsonId]
+    public Guid Id { get; set; }
+    public string email { get; set; }
+    public string password { get; set; }
+}
+public class MongoCRUD
+{
+    private IMongoDatabase db;
+    public MongoCRUD(string database)
+    {
+        var client = new MongoClient();
+        db = client.GetDatabase(database);
+    }
+    public void InsertRecords<T>(string table, T record)
+    {
+        var collection = db.GetCollection<T>(table);
+        collection.InsertOne(record);
+    }
+}
---
+++ NEA Project - Keanu/globalvariables.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+public class globals
+{
+    public string email { get; set; }
+    public string defaultemail = "@gmail.com";
+    public string password { get; set; }
+    public string usblocation { get; set; }
+    public string askuserforemail()
+    {
+        string input;
+        Console.WriteLine(" ** Please enter your email address! ** ");
+        input = Console.ReadLine();
+        email = input;
+        return email;
+    }
+
+    public string askuserforpass() // method to ask user to input password
+    {
+        string input;
+        Console.WriteLine("Enter your email password to confirm send!");
+        input = Console.ReadLine();
+        password = input;
+        return password; // returns password
+    }
+
+    public string askuserforusblocation()
+    {
+        FolderBrowserDialog file1 = new FolderBrowserDialog();
+        file1.ShowNewFolderButton = true;
+        file1.ShowDialog();
+        usblocation = file1.SelectedPath;
+        return usblocation;
+    }
+}
